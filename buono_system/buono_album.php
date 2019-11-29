553866e12ca3bcc81574e61205e54c553de3c189
ShinyaKobukai <kobukai.0415@gmail.com>
2019-11-29 13:47:07 +0900

    album updata

--- buono_system/buono_album.php
+++ buono_system/buono_album.php
@@ -1,14 +1,43 @@
+<?php  
+
+	$dsn = 'mysql:host=localhost;dbname=buono;character=utf8';
+	$user = 'root';
+	$password = '';
+
+	try{
+		$db = new PDO($dsn,$user,$password,array(PDO::ATTR_EMULATE_PREPARES=>false));
+		$posid_stmt = $db->prepare(
+			"SELECT A.post_id,B.data FROM post as A,photo_data As B WHERE A.post_id = B.post_id"
+		);
+		$posid_stmt->execute();
+		
+
+		}catch(PDOEXception $e){
+			die('エラー：'.$e->getMessage());
+	}
+
+?>
+
 <html lang="jp">
 <head>
-    <meta http-equiv="Content-type" content="text/html;charset="UTF-8"/>
+    <meta charset="utf-8"/>
     <title>buono</title>
 </head>
 <body>
     <h1>buono</h1>
-    <p><font size="20">制作中です</font></p>
-        <p>
-            <a href="buono_index.php">トップページに戻る</a>
-            <a href="buono_write.php">投稿する</a>
-        </p>
+<?php
+	//var_dump($posid_stmt);
+	while ($result = $posid_stmt->fetch()):
+?>
+    <p><?php 
+			if(empty($result["data"]) == null){
+				echo '<p><img src="data:image/jpeg;base64,' . $result["data"] . '" width="90%" height="auto"></p>'; 
+			}
+	?></p>
+<?php endwhile;?>
+    <p>
+        <a href="buono_index.php">トップページに戻る</a>
+        <a href="buono_write.php">投稿する</a>
+    </p>
 </body>
 </html>
