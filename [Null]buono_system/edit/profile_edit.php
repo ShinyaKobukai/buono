c3b0ff77e537f3f2db3fbf31f54ae4d47810409c
ShinyaKobukai <42952490+ShinyaKobukai@users.noreply.github.com>
2019-12-19 12:38:59 +0900

    Add files via upload

--- [Null]buono_system/edit/profile_edit.php
+++ [Null]buono_system/edit/profile_edit.php
@@ -1,35 +1,37 @@
-<?php 
-    if(isset( $_GET['msg'])){
-    $er_msg = $_GET['msg'];
-  }else{
-    $er_msg = '';
-  }
+<?php
+	include_once("../common/db_connect.php");
+	session_start();
+	$user_id = $_SESSION['user_id'];
+	if(isset($_POST['edit'])){
+   		if ( empty($_POST['user_name']) ) {
+     		$_SESSION["msg"] = '入力してください';
+     		print("入力してください");
+     		header('Location: profile_edit.html');
+    		exit;
+ 		}
+		foreach($_POST as $key => $val){
+			$$key=trim(htmlspecialchars($val)); 
+		}
+	if( isset($_FILES["user_icon"]) ){
+	    $user_icon = file_get_contents($_FILES["user_icon"]["tmp_name"]);
+	    $user_icon = str_replace("data:image/jpeg;base64,","",$user_icon);
+	    $user_icon = base64_encode($user_icon);
+  	}
+	try {
+			$pdo = db_connect();
+			$sql = "UPDATE user SET user_name = ?, icon = ? WHERE user_id = ?";
+			$stmt = $pdo->prepare($sql);
+    		$stmt->execute(array($user_name,$user_icon,$user_id));
+
+			if (empty($user_icon)) {
+				$_SESSION["msg"] = 'アイコンが設定されませんでした。';
+				header('Location: profile_edit.php');
+    			exit;
+			}
+
+		} catch (PDOException $e) {
+			exit('データベース接続失敗。'.$e->getMessage());
+		}
+		header('Location: ../post_list.php');
+	}
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-	<meta charset="UTF-8">
-	<title>profile edit</title>
-	<link rel="stylesheet" href="../css/buono.css" />
-</head>
-<body>
-  <header><img src="../image/logo.png" alt=""></header>
-  <div id="form_div">
-    <br/>
-    <h1>プロフィール設定</h1>
-    <form action="profile_editdb.php" method="POST" id="form" enctype="multipart/form-data">
-        新しいユーザー名　:　<input type="text" name="user_name" value="" /><br/>
-        <br/>
-        <p>アイコンを選択してください</p><input name="user_icon" type="file"><br/>
-        <br/>
-		    現在のユーザーID　:　<input type="text" name="user_id" value="" /><br/>
-        <br/>
-        現在のパスワード　:　<input type="password" name="password" value="" /><br/>
-        <input type="submit" name="edit"  value="設定する"  /><br /><br/>
-       	<br/>
-    </form>
-  </div>
-  <a href="../buono_index.html">トップページに戻る</a></br>
-  <a href="../buono_main.php">投稿する</a>
-</body>
-</html>
