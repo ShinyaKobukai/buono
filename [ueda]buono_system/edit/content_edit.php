7182b118595e5f4bdd9a8ec7def0a6b8813a4f3c
ShinyaKobukai <42952490+ShinyaKobukai@users.noreply.github.com>
2019-12-19 12:40:43 +0900

    Add files via upload

--- [ueda]buono_system/edit/content_edit.php
+++ [ueda]buono_system/edit/content_edit.php
@@ -6,27 +6,41 @@
     $content = strval($_GET['content']);
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
-    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-    <title>buono 編集</title>
-    <link rel="stylesheet" type="text/css" href="../css/buono.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/common.css" type="text/css">
+    <link rel="stylesheet" href="../css/register.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
+    <title>Buono -投稿編集-</title>
 </head>
 <body>
-    <header>
-        <img src="../image/logo.png" alt="">
-    </header>
-        <div id="post">
-        <div id ="menu_name">
-            <form action="post_edit.php?post_id=<?php echo $post_id?>" method="post">
-        </div>
-            <div id ="review"><p>編集</p></div>
-            <div id ="write">
-                <textarea name="content_editdb"><?php echo $content;?></textarea> 
-            </div>
-            <p><input type="submit" value="編集する"></p>
-        <br /><a href="../buono_main.php">投稿一覧へ</a>
-            </form>
-    </div>
+  <header>
+    <nav id="menu">
+      <ul>
+        <li><a href="../home.html"><i class="fas fa-home"></i>ホーム</a></li>
+        <li><a href="register.html"><i class="fas fa-user"></i>アカウント作成</a></li>
+        <li><a href="login.html"><i class="fas fa-sign-in-alt"></i>ログイン</a></li>
+	<li><a href="../post_list.php"><i class="far fa-comments"></i>ポスト</a></li>
+      </ul>
+    </nav>
+  </header>
+<main>
+  <div class="regi_info">
+    <h1>レビュー編集</h1>
+    <form action="edit_result.php" method="post">
+      <label for="box">review</label>
+      <textarea name="post_edit" cols="15" rows="4" maxlength="20" placeholder="ご意見・ご感想をご記入ください"></textarea>
+      <input type="submit" value="編集する">           
+    </form>
+  </div>
+</main>
+  <footer>
+    <address>&copy;2019 buono All Rights Reserved.</address>
+  </footer>
+  <script src="../js/all.js"></script>
 </body>
 </html>
+
